49f8e73f2756096f841a03f602ccb8c076abe5c7
Reclaimers <floran.nicoletti38@gmail.com>
2024-03-28 22:35:23 +0100

    Adding python server + update Gitignore

--- CNN/Pathfinding.py
+++ CNN/Pathfinding.py
@@ -2,12 +2,28 @@
 import numpy as np
 import threading as th
 import concurrent.futures as ThreadManager
- 
-
+#importing function
 from function.fun_matrix import *
+#importing server
+from Server.server import *
 
 #Equivalent of the main function
 #pathfindingAStar(matrix[0][0], matrix[4][4])
+
+hostName = "localhost"
+serverPort = 8080
+
+if __name__ == "__main__":        
+    webServer = WebRequestHandler((hostName, serverPort), )
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
 
 with ThreadManager.ThreadPoolExecutor() as executor:
     #Starting threads with a pathfinding calculation
