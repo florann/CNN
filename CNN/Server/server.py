8759ef1350a3bb8e2d186907cd6bc08972d84025
Reclaimers <floran.nicoletti38@gmail.com>
2024-04-02 20:17:32 +0200

    Setting up a server GET response + html index

--- CNN/Server/server.py
+++ CNN/Server/server.py
@@ -4,9 +4,34 @@ from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import parse_qsl, urlparse
 
+import string
+
 import time
 
+pathPage = "./Server/Pages/"
+
 class WebRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        #Getting the page name
+        splitedUrl = self.url.path.split("/",1)
+        print(splitedUrl)
+        pageRequest = pathPage+splitedUrl[1]
+
+        #Using context manager to access to the file
+        with open(pageRequest) as file:
+            #Getting the content
+            content = file.read()
+            #Writing it
+            self.wfile.write(content.encode('utf-8'))
+
+
     @cached_property
     def url(self):
         return urlparse(self.path)
