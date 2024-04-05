3c0f767ec365b92a9d7e3dd99300a33b7591176d
Reclaimers <43747832+florann@users.noreply.github.com>
2024-04-05 13:07:44 +0000

    Attempt to make both server communicate

--- CNN/React/visualizor/app/client_component/client.client.tsx
+++ CNN/React/visualizor/app/client_component/client.client.tsx
@@ -5,7 +5,7 @@ function Client() {
   const [message, setMessage] = useState('');
 
   const fetchMessage = async () => {
-    const response = await fetch('http://localhost:5000/run-script');
+    const response = await fetch('https://studious-yodel-g74wx6wx9qqhvw5v-5000.app.github.dev/',{ method: 'get', mode: 'cors' });
     const data = await response.json();
     setMessage(data.message);
   };
